06e04360d8c5b9ed7341eb8f1ce42d382bc199e6
unknown <patelgaurav282000@gmail.com>
2024-07-12 13:09:02 +0530

    Login_Signup_in_react_native in UI

---
+++ screens/Signup.js
@@ -0,0 +1,313 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  Input,
+  NativeBaseProvider,
+  Button,
+  Icon,
+  Box,
+  Image,
+  AspectRatio,
+} from "native-base";
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+function Signup() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <View style={styles.Middle}>
+        <Text style={styles.LoginText}>Signup</Text>
+      </View>
+      <View style={styles.text2}>
+        <Text>Don't have an account ? </Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.signupText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      {/* username or Email Input Field */}
+      <View style={styles.buttonStyle}>
+        <View style={styles.emailInput}>
+          <Input
+            InputLeftElement={
+              <Icon
+                as={<FontAwesome5 name="user-secret" />}
+                size="sm"
+                m={2}
+                _light={{
+                  color: "black",
+                }}
+                _dark={{
+                  color: "gray.300",
+                }}
+              />
+            }
+            variant="outline"
+            placeholder="Username or Email"
+            _light={{
+              placeholderTextColor: "blueGray.400",
+            }}
+            _dark={{
+              placeholderTextColor: "blueGray.50",
+            }}
+          />
+        </View>
+      </View>
+      {/* email */}
+
+      <View style={styles.buttonStyleX}>
+        <View style={styles.emailInput}>
+          <Input
+            InputLeftElement={
+              <Icon
+                as={<MaterialCommunityIcons name="email" />}
+                size="sm"
+                m={2}
+                _light={{
+                  color: "black",
+                }}
+                _dark={{
+                  color: "gray.300",
+                }}
+              />
+            }
+            variant="outline"
+            placeholder="Email"
+            _light={{
+              placeholderTextColor: "blueGray.400",
+            }}
+            _dark={{
+              placeholderTextColor: "blueGray.50",
+            }}
+          />
+        </View>
+      </View>
+      {/* Password Input Field */}
+      <View style={styles.buttonStyleX}>
+        <View style={styles.emailInput}>
+          <Input
+            InputLeftElement={
+              <Icon
+                as={<FontAwesome5 name="key" />}
+                size="sm"
+                m={2}
+                _light={{
+                  color: "black",
+                }}
+                _dark={{
+                  color: "gray.300",
+                }}
+              />
+            }
+            variant="outline"
+            secureTextEntry={true}
+            placeholder="Password"
+            _light={{
+              placeholderTextColor: "blueGray.400",
+            }}
+            _dark={{
+              placeholderTextColor: "blueGray.50",
+            }}
+          />
+        </View>
+      </View>
+      {/* confirm Password */}
+
+      <View style={styles.buttonStyleX}>
+        <View style={styles.emailInput}>
+          <Input
+            InputLeftElement={
+              <Icon
+                as={<FontAwesome5 name="key" />}
+                size="sm"
+                m={2}
+                _light={{
+                  color: "black",
+                }}
+                _dark={{
+                  color: "gray.300",
+                }}
+              />
+            }
+            variant="outline"
+            secureTextEntry={true}
+            placeholder="Confirm Password"
+            _light={{
+              placeholderTextColor: "blueGray.400",
+            }}
+            _dark={{
+              placeholderTextColor: "blueGray.50",
+            }}
+          />
+        </View>
+      </View>
+      {/* Button */}
+      <View style={styles.buttonStyle}>
+        <Button style={styles.buttonDesign}>Login</Button>
+      </View>
+
+      {/* Line */}
+      <View style={styles.lineStyle}>
+        <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+        <View>
+          <Text style={{ width: 50, textAlign: "center" }}>Or</Text>
+        </View>
+        <View style={{ flex: 1, height: 1, backgroundColor: "black" }} />
+      </View>
+
+      {/* Box */}
+
+      <View style={styles.boxStyle}>
+        <Box
+          onPress={() => navigation.navigate("#")}
+          style={{ height: 80, width: 80, marginLeft: 20 }}
+          shadow={3}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+          _dark={{
+            backgroundColor: "gray.700",
+          }}
+        >
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              roundedTop="lg"
+              source={{
+                uri: "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
+              }}
+              alt="Google_image"
+            />
+          </AspectRatio>
+        </Box>
+
+        <Box
+          onPress={() => navigation.navigate("#")}
+          style={{ height: 80, width: 80, marginLeft: 20 }}
+          shadow={3}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+          _dark={{
+            backgroundColor: "gray.700",
+          }}
+        >
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              roundedTop="lg"
+              source={{
+                uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjvzC_QRv6moAhgNb5C6e3yicKgFND1g2RwA&s",
+              }}
+              alt="facebook_image"
+            />
+          </AspectRatio>
+        </Box>
+
+        <Box
+          onPress={() => navigation.navigate("#")}
+          style={{ height: 80, width: 80, marginLeft: 20 }}
+          shadow={3}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+          _dark={{
+            backgroundColor: "gray.700",
+          }}
+        >
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              roundedTop="lg"
+              source={{
+                uri: "https://www.iconpacks.net/icons/2/free-twitter-logo-icon-2429-thumb.png",
+              }}
+              alt="twitter_image"
+            />
+          </AspectRatio>
+        </Box>
+
+        <Box
+          onPress={() => navigation.navigate("#")}
+          style={{ height: 80, width: 80, marginLeft: 20 }}
+          shadow={3}
+          _light={{
+            backgroundColor: "gray.50",
+          }}
+          _dark={{
+            backgroundColor: "gray.700",
+          }}
+        >
+          <AspectRatio ratio={1 / 1}>
+            <Image
+              roundedTop="lg"
+              source={{
+                uri: "https://cdn-icons-png.flaticon.com/512/154/154870.png",
+              }}
+              alt="Apple_image"
+            />
+          </AspectRatio>
+        </Box>
+      </View>
+    </View>
+  );
+}
+
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Signup />
+    </NativeBaseProvider>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  LoginText: {
+    marginTop: 100,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  Middle: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text2: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 5,
+  },
+  signupText: {
+    fontWeight: "bold",
+  },
+  emailInput: {
+    marginTop: 10,
+    marginLeft: 15,
+  },
+  buttonStyle: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  buttonStyleX: {
+    marginTop: 12,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  buttonDesign: {
+    backgroundColor: "#026efd",
+  },
+  lineStyle: {
+    flexDirection: "row",
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    alignItems: "center",
+  },
+  boxStyle: {
+    flexDirection: "row",
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15,
+    justifyContent: "space-around",
+  },
+});
